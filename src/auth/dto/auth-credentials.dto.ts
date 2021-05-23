e8a3446f830d774a833dcac151c44ebb53c9d399
Ron <ron.houben85@gmail.com>
2021-05-23 20:47:00 +0200

    feat: added validation to signUp function

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,11 +1,16 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
-  @IsNotEmpty()
-  @IsEmail()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
-  @IsNotEmpty()
   @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is too weak!',
+  })
   password: string;
 }
