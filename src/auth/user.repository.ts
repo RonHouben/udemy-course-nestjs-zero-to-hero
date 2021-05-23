430b09197cb184f0973210cfcc5d0a82b90d8464
Ron <ron.houben85@gmail.com>
2021-05-23 22:13:56 +0200

    feat: created signIn with password validation

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -32,6 +32,20 @@ export class UserRepository extends Repository<User> {
     }
   }
 
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<User['username']> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+
   private async hashPassword(password: string, salt: string): Promise<string> {
     return bcrypt.hash(password, salt);
   }
