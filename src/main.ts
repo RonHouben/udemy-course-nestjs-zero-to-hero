281f67bf9694c250b6086b51e824fd4a9776ea34
Ron <ron.houben85@gmail.com>
2021-05-24 20:27:49 +0200

    feat: enable CORS and also added launch.json for debugging

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,11 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
 
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   await app.listen(port);
 
   logger.log(`Application listening on http://localhost:${port}`);
